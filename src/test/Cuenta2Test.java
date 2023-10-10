3d9e1fa44e7ffd1d19d2ff638d55defa3f42dc7f
moren <moren@LAPTOP-FF64IE7I>
2023-10-10 19:10:34 +0200

    TDD paso1, creación de pruebas

---
+++ src/test/Cuenta2Test.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Cuenta2Test {
+	
+	Cuenta2 ctaPrueba;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		ctaPrueba=new Cuenta2();
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		ctaPrueba.ingresar(300);
+		assertEquals(300, ctaPrueba.getSaldo());
+	}
+	
+	@Test
+	void testRetirar() {
+		ctaPrueba.retirar(300);
+		assertEquals(0, ctaPrueba.getSaldo());
+	}
+
+}
